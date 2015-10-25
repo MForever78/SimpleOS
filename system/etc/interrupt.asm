788129a392f9d5a848184f3a797f6a447fcfb5cf
MForever78 <shunjian1128@gmail.com>
2015-10-25 21:30:53 +0800

    Change interrupt vector & add syscall

--- system/etc/interrupt.asm
+++ system/etc/interrupt.asm
@@ -3,33 +3,26 @@ int0:
     j _init
 
 int_clock:
-int0:
-    dd 0
-
-int_keyboard:
 int1:
     dd 0
 
-int_res_0:
+int_vga:
 int2:
     dd 0
 
-int_res_2:
+int_board:
 int3:
     dd 0
 
-int_res_3:
+int_keyboard:
 int4:
     dd 0
 
-int_res_4:
+int_uart:
 int5:
     dd 0
 
-int_res_5:
+int_spi:
 int6:
     dd 0
 
-int_res_6:
-int7:
-    dd 0
