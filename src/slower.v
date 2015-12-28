dfeda8581957eb5d9c98b230144002332a26c302
MForever78 <shunjian1128@gmail.com>
2015-12-29 01:10:53 +0800

    minor fix here and there

--- src/slower.v
+++ src/slower.v
@@ -12,7 +12,7 @@ module slower(
             cnt1s <= 0;
             clk1s <= 0;
         end else begin
-            if (cnt1s == 24999999) begin
+            if (cnt1s == 12499999) begin
                 clk1s <= ~clk1s;
                 cnt1s <= 0;
             end else
@@ -25,7 +25,7 @@ module slower(
             cnt2s <= 0;
             clk2s <= 0;
         end else begin
-            if (cnt2s == 49999999) begin
+            if (cnt2s == 24999999) begin
                 clk2s <= ~clk2s;
                 cnt2s <= 0;
             end else
